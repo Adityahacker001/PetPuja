ccd51b47133c5472e9865c91a7ef6e957bec5e3d
adityasingh001 <141174615+Adityahacker001@users.noreply.github.com>
2025-06-06 10:01:57 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState("{}");
     const [iteminfo, setIteminfo] = useState([])
-    const url = "http://localhost:4000"
+    const url = "https://petpuja-backend-ww2v.onrender.com"
     const [token, setToken] = useState("")
     const [food_list, setFood_list]=useState([])
 
